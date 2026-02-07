4df10179e3847070782701d85ab40c863ee8f0a6
HazimKhairi <hazim4128@gmail.com>
2026-02-07 17:18:49 +0800

    setup push api

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,1 +1,10 @@
-import ChromaClient from ChromaClient;
+const { ChromaClient } = require('chromadb');
+const client = new ChromaClient({ path: process.env.CHROMADB_URL || 'localhost:8000'})
+async function getCollection() {
+  return await client.getCollection({
+    name: 'hr-knowledge',
+    embeddingFunction: null
+  })
+}
+
+module.exports = { client,getCollection}
